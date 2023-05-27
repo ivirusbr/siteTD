80e2f8d155a22e73ae0af3c2b500fc4f281844b9
ivirusbr <popvisualcontato@gmail.com>
2023-05-27 19:06:52 -0300

    atualização
    atualização

---
+++ site-cliente/javascript/script.js
@@ -0,0 +1,25 @@
+// select
+const todoForm = document.querySelector("#todo-formulario");
+const todoInput = document.querySelector("#todo-input");
+
+
+
+// function
+
+
+
+//events
+ 
+todoForm.addEventListener( "submit", (e) => {
+    e.preventDefault();
+
+    console.log("deu certo");
+const valueInput = todoInput.value;
+
+if (valueInput = true){
+
+    console.log(valueInput)
+}
+
+
+});
